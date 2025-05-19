d3cad701d69816056ba50791703b74cd31db69dd
Edwin Mendez <mp19034@ues.edu.sv>
2025-05-19 17:27:53 -0600

    se finalizo el index para pc y celular

--- src/main.js
+++ src/main.js
@@ -1,24 +1,65 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./css/style.css";
+import javascriptLogo from "./assets/javascript.svg";
+import cssLogo from "./assets/css.square.svg"
+import htmlLogo from "./assets/html.svg"
+import accesibilidadLogo from "./assets/accessibility.svg";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+
+document.querySelector("#app").innerHTML = `
+  <div class="grid-container">
+    <section class="presentacion">
+     <h1>Welcome to the <b>Frontend Quizz!</b></h1>
+      <h4>Pick a subject to get started</h4>
+    </section>
+ 
+    <section class="buttonsquizz">
+      <section class="quizzHtml">
+        <img src="${htmlLogo}" alt="JavaScript" />
+        <a><b>HTML</b></a>   
+      </section>
+      <section class="quizzJs">
+        <img src="${javascriptLogo}" alt="JavaScript" />
+        <a><b>Javascript</b></a> 
+      </section>
+      <section class="quizzCss">
+        <img 
+          src="${cssLogo}" 
+          alt="JavaScript" 
+          id="js-btn"
+          style="cursor: pointer;" 
+        /> <a><b>CSS</b></a> 
+      </section>
+      <section class="quizzAccesibilidad">
+        <img 
+          src="${accesibilidadLogo}" 
+          alt="JavaScript" 
+          id="js-btn"
+          style="cursor: pointer;" 
+        /> <a><b>Accessibility</b></a> 
+      </section>
+    </section>
+    
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+document.querySelector(".quizzJs").addEventListener("click", () => {
+  alert("¡Elegiste JavaScript!");
+});
+
+document.querySelector(".quizzCss").addEventListener("click", () => {
+  alert("¡Elegiste css!");
+});
+
+document.querySelector(".quizzHtml").addEventListener("click", () => {
+  alert("¡Elegiste html!");
+});
+
+document.querySelector(".quizzAccesibilidad").addEventListener("click", () => {
+  alert("¡Elegiste accesibilidad!");
+});
+
+const toggle = document.getElementById("toggle-theme");
+
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("light-theme");
+});
